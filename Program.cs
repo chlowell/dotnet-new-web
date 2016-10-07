88dec3d0849ae3d74a38493f16e114d5788fa916
chlowell <chlowe@microsoft.com>
2016-10-06 20:45:25 -0700

    Hardcode URL

--- Program.cs
+++ Program.cs
@@ -16,6 +16,7 @@ namespace WebApplication
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
+                .UseUrls("http://*:8080")
                 .Build();
 
             host.Run();
